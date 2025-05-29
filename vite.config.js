0c0aea9f5887cd988b611716800d964887643b41
LZBZ-1 <jalb200312@gmail.com>
2025-05-28 23:52:38 -0500

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://LZBZ-1.github.io/flashapp',
+  base: 'https://lzbz-1.github.io/flashapp',
 })
